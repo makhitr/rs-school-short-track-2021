f79867d812ffd883e95fdda1a6156b1c253ba6fc
makhitr <77505619+makhitr@users.noreply.github.com>
2021-04-12 23:02:48 +0300

    Update 02-common-character-count.js

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -18,7 +18,7 @@ function getCommonCharacterCount(s1, s2) {
     for (let j = 0; j < splited2.length; j++) {
       if (splited1[i] === splited2[j]) {
         count += 1;
-        splited2.splice(splited2[j], 1);
+        splited2.splice(j, 1);
         j = splited2.length + 1;
       }
     }
